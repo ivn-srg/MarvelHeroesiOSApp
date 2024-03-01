2fd45fed75c1a0beec4ccf1ddf5983a766fad43b
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2024-03-01 18:31:32 +0600

    adding UIColor's extension and fill background color on screen

--- MarvelHeroesApp/ViewController.swift
+++ MarvelHeroesApp/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    private lazy var viewCn: UIView = {
+        let vc = UIView()
+        vc.translatesAutoresizingMaskIntoConstraints = false
+        return vc
+    }()
 
+    // MARK: - lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
+        
+        setupView()
     }
 
-
+    private func setupView() {
+        
+        viewCn.backgroundColor = UIColor(rgb: 0x2b272b)
+        
+        view.addSubview(viewCn)
+        
+        NSLayoutConstraint.activate([
+            viewCn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            viewCn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            viewCn.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewCn.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
